6c6dfbd1846dd20c0823600a3621ca0140a11eaa
DerekWang <wiwj1987ww@163.com>
2017-05-22 20:33:26 +0800

    Update connectMount.js

--- connectMount.js
+++ connectMount.js
@@ -7,3 +7,34 @@ connect()
   .use('/admin',admin)
   .use(hello)
   .listen(3000);
+
+
+function restrict(req,res,next){
+  var authorization = req.headers.authorization;
+  if(!authorization) return next(new Error('Unauthorized'));
+  var parts = authorization.split(' ');
+  var scheme = parts[0];
+  var auth =new BUffer(parts[1],'base64').toString().split(':');
+  var user = auth[0];
+  var pass = auth[1];
+  authenticateWithDatabase(user,pass,function(err){
+  //根据数据库中的记录检查信息的函数
+    if(err) return next(err); //告诉分析器出错了
+    
+    //用error做参数的next()
+    
+    next(); //如果认证信息有效，不带参数调用next()
+  });
+}
+
+function admin(req,res,next){
+  switch(req.url){
+    case '/':
+      res.end('try /users');
+      break;
+    case '/users':
+      res.setHeader('Content-Type','application/json');
+      res.end(JSON.stringify(['tobi','loki','jane']));
+      break;
+   }
+}
